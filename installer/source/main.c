4ae9810f711100561c8080f235d857b13686025d
ac2pic <ac2pic@gmail.com>
2022-01-03 21:49:31 -0600

    feat:  Add USB detection

--- installer/source/main.c
+++ installer/source/main.c
@@ -7,6 +7,34 @@
 
 #define	MNT_UPDATE	0x0000000000010000ULL /* not real mount, just update */
 
+int getUsbMountIndex() {
+    char targetPath[128];
+    memset(targetPath, 0, sizeof(targetPath));
+    int usbId = -1;
+    int failedCount = 0;
+    while (1) {
+        for(int i = 0; i < 8; i++) {
+            sprintf(targetPath, "/mnt/usb%i/data/daemons/installer/eboot.bin", i);
+            int fd = open(targetPath, O_RDONLY, 0644);
+            close(fd);
+            if (fd > 0) {
+                usbId = i;
+                goto exit;
+            }
+        }
+        failedCount++;
+        if (failedCount >= 5) {
+            break;
+        }
+        sceKernelSleep(1);
+    }
+
+exit:
+    return usbId;
+
+}
+
+
 int _main() {
     initKernel();
     initLibc();
@@ -14,7 +42,22 @@ int _main() {
     jailbreak();
 
     initSysUtil();
-    // int ret = mount_large_fs("/dev/da0x4.crypt", "/system", "exfatfs", "511", MNT_UPDATE);
-    printf_notification("Test");
+    do {
+        int ret = mount_large_fs("/dev/da0x4.crypt", "/system", "exfatfs", "511", MNT_UPDATE);
+        if (ret < 0) {
+            printf_notification("Failed to mount /system for RW. - %08li", ret);
+            break;
+        } else {
+            printf_notification("Successfully mounted /system for RW");
+        }
+        int usbIndex = getUsbMountIndex();
+        if (usbIndex == -1) {
+            printf_notification("Fail to find usb.");
+            break;
+        } else {
+            printf_notification("Found match at  /mnt/usb%i/", usbIndex);
+        }
+    } while (0);
+
     return 0;
 }
